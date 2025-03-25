d39f1dae1318a998e960b409b1293a5f5d091ee0
jusanchez6 <julian.sanchez6@udea.edu.co>
2025-03-25 13:49:58 -0500

    driver del sesnsor magneticoy funcionalidad del encoder magnetico

--- as5600/main/as5600.cpp
+++ as5600/main/as5600.cpp
@@ -10,43 +10,29 @@
 #define I2C_MASTER_NUM I2C_NUM_0
 #define I2c_SLAVE_ADDR 0x36
 
-static const char *TAG = "AS5600";
 
-void print_binary(uint8_t value) {
-    printf("0b");
-    for (int i = 7; i >= 0; i--) {  // Recorre los 8 bits de mayor a menor
-        printf("%d", (value >> i) & 1);
-    }
-    printf("\n");
-}
-
-
-void scan_i2c_devices(i2c_port_t i2c_num) {
-    for (uint8_t addr = 1; addr < 127; addr++) {
-        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-        i2c_master_start(cmd);
-        i2c_master_write_byte(cmd, (addr << 1) | I2C_MASTER_WRITE, true);
-        i2c_master_stop(cmd);
-
-        esp_err_t ret = i2c_master_cmd_begin(i2c_num, cmd, pdMS_TO_TICKS(100));
-        i2c_cmd_link_delete(cmd);
-
-        if (ret == ESP_OK) {
-            printf("Device found at address 0x%02X\n", addr);
-        } else {
-            printf("No device found at address 0x%02X\n", addr);
-        }
-    }
-}
 
 class AS5600
 {
 public:
+
+    /**
+     * @brief Construct a new AS5600 object
+     * 
+     * @param i2c_num I2C port number
+     * @param sda GPIO pin for SDA
+     * @param scl GPIO pin for SCL
+     */
     AS5600(i2c_port_t i2c_num, gpio_num_t sda, gpio_num_t scl)
         : i2c_num(i2c_num), sda(sda), scl(scl) {}
 
+    /**
+     * @brief Initialize the AS5600
+     *  
+     * @return esp_err_t 
+     */
     esp_err_t begin()
-    {
+        {
         // Initialize the I2C AS5600 driver
 
         i2c_config_t conf = {};
@@ -62,50 +48,95 @@ public:
         ESP_ERROR_CHECK(i2c_param_config(i2c_num, &conf));
         vTaskDelay(pdMS_TO_TICKS(100));
 
-        ESP_ERROR_CHECK(i2c_driver_install(i2c_num, conf.mode, 0, 0, ESP_INTR_FLAG_LEVEL1));
+        ESP_ERROR_CHECK(i2c_driver_install(i2c_num, conf.mode, 0, 0, 0));
         vTaskDelay(pdMS_TO_TICKS(100));
 
         return ESP_OK;
     }
 
-    esp_err_t read_angle(uint8_t *angle)
+    /**
+     * @brief Read the angle from the AS5600
+     * 
+     * @param angle Pointer to the angle variable
+     * @param raw If true, read the raw angle, otherwise read the processed angle
+     * 
+     * @return esp_err_t 
+     */
+    esp_err_t read_angle(int16_t *angle, bool raw = false)
     {
-        // Read the angle from the AS5600
 
-        uint8_t rx_data[1];
-        uint8_t reg = 0x0E;
+        uint8_t rx_data[2];
+        uint8_t reg = raw ? 0x0C : 0x0E;        // 0x0C para angulo sin procesar, 0x0E para angulo procesado
 
-        esp_err_t err = i2c_master_write_read_device(i2c_num, I2c_SLAVE_ADDR, &reg, 1, rx_data, 1, pdMS_TO_TICKS(1000));
+        esp_err_t err = i2c_master_write_read_device(i2c_num, I2c_SLAVE_ADDR, &reg, 1, rx_data, 2, pdMS_TO_TICKS(1000));
 
-        printf("error lectura del angulo: 0x%04x\n", err);
-        *angle = rx_data[0];
+        if (err != ESP_OK)
+        {
+            ESP_LOGE(TAG, "Error reading angle: %d", err);
+            return err;
+        }
 
-
+        *angle = ((rx_data[0]& 0x0F)<< 8 | rx_data[1]);
         return ESP_OK;
     }
 
+    /**
+     * @brief Detect the magnet
+     * 
+     * @return true if the magnet is detected, false otherwise
+     * 
+     * @note The magnet is detected when the bit 4 of the status register is 1
+     * 
+     */
     bool magnet_detect()
     {
-        // Detect the magnet
 
         uint8_t rx_data[1];
         uint8_t reg = 0x0B;
-
         esp_err_t err = i2c_master_write_read_device(i2c_num, I2c_SLAVE_ADDR, &reg, 1, rx_data, 1, pdMS_TO_TICKS(1000));
+        
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG, "Error reading the register 0x0B");
+            return false;
+        }
 
-        printf("rx_data[0]: 0x%02X\n", rx_data[0]);
-
-        printf("error deteccion del iman: 0x%04x\n", err);
-        print_binary(rx_data[0]);
-        //buscar en el bit 4 si es 1 detecto un magnet  
         return (rx_data[0] & 0x20) == 0x20;
 
     }
 
+    get_encoder_steps () {
+        int16_t angle;
+
+        if (read_angle(&angle) != ESP_OK) {
+            ESP_LOGE(TAG, "Error reading angle");
+            return total_steps;
+        }
+
+        int delta = angle -last_angle;
+
+
+        // Ajuste para cruce de 0° - 360°
+        if (delta > 2048)  // Rotación en sentido antihorario
+            delta -= 4096;
+        else if (delta < -2048)  // Rotación en sentido horario
+            delta += 4096;
+
+        total_steps += delta;
+        last_angle = angle;
+
+        return total_steps
+    }
+
 private:
-    i2c_port_t i2c_num;
-    gpio_num_t sda;
-    gpio_num_t scl;
+    
+    i2c_port_t i2c_num;     //!< I2C port number
+    gpio_num_t sda;         //!< GPIO pin for SDA
+    gpio_num_t scl;         //!< GPIO pin for SCL
+    int16_t last_angle;     //!< Last read of angle
+    int total_steps         //!< Counter of the total steps
+
+    static constexpr const char *TAG = "AS5600";
+
 };
 
 extern "C"
@@ -116,38 +147,5 @@ extern "C"
 void app_main(void)
 {
 
-    // detectar dispositivos I2C
-
-    // Setup
-    AS5600 sensor(I2C_NUM_0, (gpio_num_t)I2C_MASTER_SDA_IO, (gpio_num_t)I2C_MASTER_SCL_IO);
-    printf("AS5600\n");
-
-
-    ESP_ERROR_CHECK(sensor.begin());
-
-    printf("AS5600 inicializado\n");
-    uint8_t angle;
-
-    // esperar un momento
-    vTaskDelay(pdMS_TO_TICKS(1000));
-
-    scan_i2c_devices(I2C_NUM_0);
-
-    while (true)
-    {
-        sensor.read_angle(&angle);
-
-        ESP_LOGI(TAG, "Angle: %d", angle);
-        
-        if (sensor.magnet_detect())
-        {
-            ESP_LOGI(TAG, "Magnet detected");
-        }
-        else
-        {
-            ESP_LOGI(TAG, "Magnet not detected");
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+    
 }
