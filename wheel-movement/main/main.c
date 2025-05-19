4b2d9b0101120a28f3988a0a3ea6e736216e231c
Nelson Parra <nelsonfernandopg@gmail.com>
2025-05-19 10:20:53 -0500

    reforming tm driver

--- wheel-movement/main/main.c
+++ wheel-movement/main/main.c
@@ -52,8 +52,6 @@
 
 // Include ESP IDF libraries
 #include <assert.h>
-#include "esp_partition.h"
-#include "spi_flash_mmap.h"
 #include "freertos/task.h"
 
 
@@ -72,9 +70,10 @@
 ///<--------------------------------------------------
 
 ///<------------- TM151 configuration ----------------
-#define UART_TX 17                          ///< Gpio pin for UART TX
-#define UART_RX 18                          ///< GPIO pin for UART RX
-static const char* TAG_TM151 = "TM151";     ///< Tag for TM151
+#define TM151_UART_TX 17                          ///< Gpio pin for UART TX
+#define TM151_UART_RX 18                          ///< GPIO pin for UART RX
+#define TM151_UART_BAUDRATE 921600                ///< Baudrate for UART communication
+#define TM151_BUFFER_SIZE 1024                       ///< Buffer size for UART communication
 
 ///<--------------------------------------------------
 
@@ -98,123 +97,119 @@ static const char* TAG_TM151 = "TM151";     ///< Tag for TM151
 #if MAIN_MODE == 0
 
 // configuration of motors
-bldc_pwm_motor_t pwm, pwm2;
+// bldc_pwm_motor_t pwm, pwm2;
 
 // object to control the magnetic rotary encoder
-AS5600_t gAs5600;
+// AS5600_t gAs5600;
 
 // Object to control the 
-vl53l1x_t gVl53l1x;
+// vl53l1x_t gVl53l1x;
 
 // object to control the TM151
 uart_t myUART;                  ///< UART object
 
 
-float angle; ///< Angle read from the AS5600 sensor
+// float angle; ///< Angle read from the AS5600 sensor
 
 void app_main(void)
 {
     ///<------------- Initialize the AS5600 sensor -------
-    AS5600_Init(&gAs5600, AS5600_I2C_MASTER_NUM, AS5600_I2C_MASTER_SCL_GPIO, AS5600_I2C_MASTER_SDA_GPIO, AS5600_OUT_GPIO);
+    // AS5600_Init(&gAs5600, AS5600_I2C_MASTER_NUM, AS5600_I2C_MASTER_SCL_GPIO, AS5600_I2C_MASTER_SDA_GPIO, AS5600_OUT_GPIO);
 
-    // Set some configurations to the AS5600
-    AS5600_config_t conf = {
-        .PM = AS5600_POWER_MODE_NOM, ///< Normal mode
-        .HYST = AS5600_HYSTERESIS_OFF, ///< Hysteresis off
-        .OUTS = AS5600_OUTPUT_STAGE_ANALOG_RR, ///< Analog output 10%-90%
-        .PWMF = AS5600_PWM_FREQUENCY_115HZ, ///< PWM frequency 115Hz
-        .SF = AS5600_SLOW_FILTER_16X, ///< Slow filter 16x
-        .FTH = AS5600_FF_THRESHOLD_SLOW_FILTER_ONLY, ///< Slow filter only
-        .WD = AS5600_WATCHDOG_ON, ///< Watchdog on
-    };
-    AS5600_SetConf(&gAs5600, conf);
+    // // Set some configurations to the AS5600
+    // AS5600_config_t conf = {
+    //     .PM = AS5600_POWER_MODE_NOM, ///< Normal mode
+    //     .HYST = AS5600_HYSTERESIS_OFF, ///< Hysteresis off
+    //     .OUTS = AS5600_OUTPUT_STAGE_ANALOG_RR, ///< Analog output 10%-90%
+    //     .PWMF = AS5600_PWM_FREQUENCY_115HZ, ///< PWM frequency 115Hz
+    //     .SF = AS5600_SLOW_FILTER_16X, ///< Slow filter 16x
+    //     .FTH = AS5600_FF_THRESHOLD_SLOW_FILTER_ONLY, ///< Slow filter only
+    //     .WD = AS5600_WATCHDOG_ON, ///< Watchdog on
+    // };
+    // AS5600_SetConf(&gAs5600, conf);
     
-    // Read the configuration
-    uint16_t conf_reg;
-    AS5600_ReadReg(&gAs5600, AS5600_REG_CONF_H, &conf_reg);
-    printf("Configuration register read: 0x%04X\n", conf_reg);
-    printf("Configuration register written: 0x%04X\n", conf.WORD);
+    // // Read the configuration
+    // uint16_t conf_reg;
+    // AS5600_ReadReg(&gAs5600, AS5600_REG_CONF_H, &conf_reg);
+    // printf("Configuration register read: 0x%04X\n", conf_reg);
+    // printf("Configuration register written: 0x%04X\n", conf.WORD);
 
-    AS5600_ReadReg(&gAs5600, AS5600_REG_STATUS, &conf_reg);
+    // AS5600_ReadReg(&gAs5600, AS5600_REG_STATUS, &conf_reg);
 
-    printf("Status register read: 0x%02X\n", conf_reg);
+    // printf("Status register read: 0x%02X\n", conf_reg);
 
-    AS5600_SetStartPosition(&gAs5600, 0); ///< Set the start position to 0
-    AS5600_SetStopPosition(&gAs5600, 4095); ///< Set the stop position to 4095
+    // AS5600_SetStartPosition(&gAs5600, 0); ///< Set the start position to 0
+    // AS5600_SetStopPosition(&gAs5600, 4095); ///< Set the stop position to 4095
 
-    AS5600_InitADC(&gAs5600); ///< Initialize the ADC driver
+    // AS5600_InitADC(&gAs5600); ///< Initialize the ADC driver
     ///<--------------------------------------------------
 
 
     ///<-------------- Initialize the VL53L1X sensor -----
-    if(!VL53L1X_init(&gVl53l1x, VL53L1X_I2C_PORT, VL53L1X_SCL_GPIO, VL53L1X_SDA_GPIO, 0)){
-        ESP_LOGE(TAG_VL53L1X, "Could not initialize VL53L1X sensor...");
-        return;
-    }
-    VL53L1X_setDistanceMode(&gVl53l1x, Short); 
-    VL53L1X_setMeasurementTimingBudget(&gVl53l1x, 20000);
-    VL53L1X_startContinuous(&gVl53l1x, 30);
+    // if(!VL53L1X_init(&gVl53l1x, VL53L1X_I2C_PORT, VL53L1X_SCL_GPIO, VL53L1X_SDA_GPIO, 0)){
+    //     ESP_LOGE(TAG_VL53L1X, "Could not initialize VL53L1X sensor...");
+    //     return;
+    // }
+    // VL53L1X_setDistanceMode(&gVl53l1x, Short); 
+    // VL53L1X_setMeasurementTimingBudget(&gVl53l1x, 20000);
+    // VL53L1X_startContinuous(&gVl53l1x, 30);
     ///<--------------------------------------------------
     
 
     ///<-------------- Initialize the TM151 sensor ------
-    if (!myUART.init_with_defaults(&myUART, 115200, 1024, UART_TX, UART_RX) ) {
-        ESP_LOGI(TAG_TM151, "Could not initialize  TM151 sensor... ");
-        return;
-    }
-    ESP_LOGI(TAG_TM151, "TM151 initialized! ");
+    tm151_init(&myUART, TM151_UART_BAUDRATE, TM151_BUFFER_SIZE, TM151_UART_TX, TM151_UART_RX); ///< Initialize the TM151 sensor
 
-    // Initialize the TM151 interface
-    
-    EasyProfile_C_Interface_Init();
+    float acceleration[3];
 
     ///<--------------------------------------------------
 
     ///<----------- Initialize the BLDC motor PWM --------
     // ESP_LOGI("PWM", "Starting test..."); ///< Log message
-    bldc_init(&pwm, PWM_GPIO, PWM_REV_GPIO, PWM_FREQ, 0, PWM_RESOLUTION, MIN_PWM_CAL, MAX_PWM_CAL); ///< Initialize the BLDC motor
-    bldc_enable(&pwm); ///< Enable the BLDC motor
-    bldc_set_duty(&pwm, 0); ///< Set the duty cycle to 0%
+    // bldc_init(&pwm, PWM_GPIO, PWM_REV_GPIO, PWM_FREQ, 0, PWM_RESOLUTION, MIN_PWM_CAL, MAX_PWM_CAL); ///< Initialize the BLDC motor
+    // bldc_enable(&pwm); ///< Enable the BLDC motor
+    // bldc_set_duty(&pwm, 0); ///< Set the duty cycle to 0%
     ///<--------------------------------------------------
 
-    vTaskDelay(5000 / portTICK_PERIOD_MS); ///< Wait for 15 seconds    
+    // vTaskDelay(5000 / portTICK_PERIOD_MS); ///< Wait for 15 seconds    
 
-    int16_t duty = 0; ///< Duty cycle variable
-    bool reverse = false; ///< Reverse variable
-    float last_angle = 0;
+    // int16_t duty = 0; ///< Duty cycle variable
+    // bool reverse = false; ///< Reverse variable
+    // float last_angle = 0;
 
 
-    for (uint16_t i = 0; i < 500; i++)
+    while (1)
     {
         // ESP_LOGI("PWM", "ESC running!"); ///< Log message
 
         ///<-------------- Get angle through ADC -------------
-        angle = AS5600_ADC_GetAngle(&gAs5600); ///< Get the angle from the ADC
+        // angle = AS5600_ADC_GetAngle(&gAs5600); ///< Get the angle from the ADC
         ///<--------------------------------------------------
 
-        if(i){
-            float diff = angle - last_angle;
-            if(diff < 0) diff += 360;
+        // if(i){
+        //     float diff = angle - last_angle;
+        //     if(diff < 0) diff += 360;
 
-            ESP_LOGI("Encoder_PWM", "Angle: %f, RPM: %f", angle, diff* (float)(100 / 6)); ///< Log message
-            last_angle = angle;
-        }
+        //     ESP_LOGI("Encoder_PWM", "Angle: %f, RPM: %f", angle, diff* (float)(100 / 6)); ///< Log message
+        //     last_angle = angle;
+        // }
 
         ///<-------------- Get distance through VL53L1X ------
-        ESP_LOGI(TAG_VL53L1X, "Distance %d mm", VL53L1X_readDistance(&gVl53l1x, 1)); */
+        // ESP_LOGI(TAG_VL53L1X, "Distance %d mm", VL53L1X_readDistance(&gVl53l1x, 1));
         ///<--------------------------------------------------
 
 
         ///<-------------- Get data from TM151 --------------
-        uint16_t toId = EP_ID_BROADCAST_;
-        char* txData;
-        int txSize;
+        // uint16_t toId = EP_ID_BROADCAST_;
+        // char* txData;
+        // int txSize;
 
-        if (EP_SUCC_ == EasyProfile_C_Interface_TX_Request(toId, EP_CMD_RPY_, &txData, &txSize)) {
-            uart_write(&myUART, (uint8_t*)txData, (size_t)txSize);
-        } 
+        // if (EP_SUCC_ == EasyProfile_C_Interface_TX_Request(EP_ID_BROADCAST_, EP_CMD_Raw_GYRO_ACC_MAG_, &txData, &txSize)) {
+        //     uart_write(&myUART, (uint8_t*)txData, (size_t)txSize);
+        // } 
 
-        SerialPort_DataGet(&myUART);
+        // SerialPort_DataGet(&myUART);
+        SerialPort_DataReceived_RawAcc(&myUART, acceleration);
+        ESP_LOGI(TAG_TM151, "Acceleration: %0.4f %0.4f %0.4f", acceleration[0], acceleration[1], acceleration[2]); ///< Log message
 
         ///<--------------------------------------------------
 
@@ -236,12 +231,12 @@ void app_main(void)
         //     bldc_set_duty(&pwm, duty); ///< Set the duty cycle to the current PWM value
         // }
 
-        bldc_set_duty(&pwm, 80); ///< Set the duty cycle to the current PWM value
+        // bldc_set_duty(&pwm, 80); ///< Set the duty cycle to the current PWM value
 
-        vTaskDelay(10 / portTICK_PERIOD_MS); ///< Wait for 2 seconds
+        vTaskDelay(5 / portTICK_PERIOD_MS); ///< Wait for 2 seconds
     }
 
-    bldc_set_duty(&pwm, 0); ///< Set the duty cycle to the current PWM value
+    // bldc_set_duty(&pwm, 0); ///< Set the duty cycle to the current PWM value
 
 }
 #endif
