4139764cf377014922f759934223dcf6c1015828
jusanchez6 <julian.sanchez6@udea.edu.co>
2025-07-04 10:47:03 -0500

    servidor funcional y listo para integración

--- server_example/main/server_example.c
+++ server_example/main/server_example.c
@@ -61,6 +61,88 @@ esp_err_t line_movement_handler(httpd_req_t *req) {
 }
 
 
+esp_err_t circular_movement_handler(httpd_req_t *req) {
+    char param[128];
+    ESP_LOGI(TAG, ">> Entró al handler /circularMovement");
+
+    // Configurar encabezados CORS
+    httpd_resp_set_type(req, "text/plain");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Headers", "Content-Type");
+
+    if (httpd_req_get_url_query_str(req, param, sizeof(param)) == ESP_OK) {
+        ESP_LOGI(TAG, "Query string: %s", param);
+
+        char direction[16], degrees[8], velocity[8], radius[8];
+
+        if (httpd_query_key_value(param, "direction", direction, sizeof(direction)) == ESP_OK &&
+            httpd_query_key_value(param, "degrees", degrees, sizeof(degrees)) == ESP_OK &&
+            httpd_query_key_value(param, "velocity", velocity, sizeof(velocity)) == ESP_OK &&
+            httpd_query_key_value(param, "distance", radius, sizeof(radius)) == ESP_OK) {
+
+            ESP_LOGI(TAG, ">> Params OK");
+            ESP_LOGI(TAG, "Direction: %s", direction);
+            ESP_LOGI(TAG, "Degrees: %s", degrees);
+            ESP_LOGI(TAG, "Velocity: %s", velocity);
+            ESP_LOGI(TAG, "Radius: %s", radius);
+
+            httpd_resp_sendstr(req, "Command received");
+            return ESP_OK;
+        } else {
+            ESP_LOGE(TAG, "Error obtaining parameters");
+            httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Missing params");
+            return ESP_FAIL;
+        }
+
+    } else {
+        ESP_LOGE(TAG, "Couldn't get the query string");
+        httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "No query string");
+        return ESP_FAIL;
+    }
+}
+
+esp_err_t selfRotation_movement_handler(httpd_req_t *req) {
+    char param[128];
+    ESP_LOGI(TAG, ">> Entró al handler /selfRotation");
+
+    // Configurar encabezados CORS
+    httpd_resp_set_type(req, "text/plain");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    httpd_resp_set_hdr(req, "Access-Control-Allow-Headers", "Content-Type");
+
+    if (httpd_req_get_url_query_str(req, param, sizeof(param)) == ESP_OK) {
+        ESP_LOGI(TAG, "Query string: %s", param);
+
+        char direction[16], degrees[8], velocity[8];
+
+        if (httpd_query_key_value(param, "direction", direction, sizeof(direction)) == ESP_OK &&
+            httpd_query_key_value(param, "degrees", degrees, sizeof(degrees)) == ESP_OK &&
+            httpd_query_key_value(param, "velocity", velocity, sizeof(velocity)) == ESP_OK ) {
+
+            ESP_LOGI(TAG, ">> Params OK");
+            ESP_LOGI(TAG, "Direction: %s", direction);
+            ESP_LOGI(TAG, "Degrees: %s", degrees);
+            ESP_LOGI(TAG, "Velocity: %s", velocity);
+
+            httpd_resp_sendstr(req, "Command received");
+            return ESP_OK;
+        } else {
+            ESP_LOGE(TAG, "Error obtaining parameters");
+            httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Missing params");
+            return ESP_FAIL;
+        }
+
+    } else {
+        ESP_LOGE(TAG, "Couldn't get the query string");
+        httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "No query string");
+        return ESP_FAIL;
+    }
+}
+
+
+
 // Inicializa el modo AP
 void wifi_init_softap(void) {
     ESP_LOGI(TAG, "Inicializando WiFi en modo Access Point...");
@@ -106,6 +188,23 @@ httpd_handle_t start_webserver(void) {
             .user_ctx = NULL
         };
         httpd_register_uri_handler(server, &uri_line_movement);
+
+        httpd_uri_t uri_circular_movement = {
+            .uri = "/circularMovement",
+            .method = HTTP_GET,
+            .handler = circular_movement_handler,
+            .user_ctx = NULL
+        };
+        httpd_register_uri_handler(server, &uri_circular_movement);
+
+        httpd_uri_t uri_self_rotation = {
+            .uri = "/selfRotation",
+            .method = HTTP_GET,
+            .handler = selfRotation_movement_handler,
+            .user_ctx = NULL
+        };
+        httpd_register_uri_handler(server, &uri_self_rotation);        
+
         ESP_LOGI(TAG, "Servidor web iniciado en modo AP");
     }
 
